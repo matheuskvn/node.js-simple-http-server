95846680dbeacf81196b679408e7cec33a771fdb
matheuskvn <matheus.kbm@gmail.com>
2025-06-14 06:00:00 +0200

    Creating a HTTP server on port 3000, also installed nodemon

---
+++ simpleServer.js
@@ -0,0 +1,13 @@
+import http from 'http';
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+    res.write(`<h1>Hello, this is my first Node.js server and it's currently running on port ${PORT}</h1>`);
+    res.end();
+});
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
